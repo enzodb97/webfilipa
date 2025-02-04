aba2d4f29c778f544ae94a0dab0d6ed1451d38b7
Enzo Daniel Bertolusso <151404661+enzodb97@users.noreply.github.com>
2025-02-04 16:54:41 -0300

    add components proyects and images

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,105 +1,108 @@
-import filipap from './filipap.png'
-import filipan from './filipan.png'
-import logo from './logo.ico'
-import logo_dark from './logo_dark.svg'
-import cross_icon from './cross_icon.svg'
-import menu_icon from './menu_icon.svg'
-import star_icon from './star_icon.svg'
-import left_arrow from './left_arrow.svg'
-import right_arrow from './right_arrow.svg'
-import header_img from './header_img.png'
-import brand_img from './brand_img.jpeg'
-import project_img_1 from './project_img_1.jpg'
-import project_img_2 from './project_img_2.jpg'
-import project_img_3 from './project_img_3.jpg'
-import project_img_4 from './project_img_4.jpg'
-import project_img_5 from './project_img_5.jpg'
-import project_img_6 from './project_img_6.jpg'
-import profile_img_1 from './profile_img_1.png'
-import profile_img_2 from './profile_img_2.png'
-import profile_img_3 from './profile_img_3.png'
+import filipap from "./filipap.png";
+import filipan from "./filipan.png";
+import logo from "./logo.ico";
+import logo_dark from "./logo_dark.svg";
+import cross_icon from "./cross_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import star_icon from "./star_icon.svg";
+import left_arrow from "./left_arrow.svg";
+import right_arrow from "./right_arrow.svg";
+import header_img from "./header_img.png";
+import brand_img from "./brand_img.jpeg";
+import project_img_1 from "./project_img_1.jpeg";
+import project_img_2 from "./project_img_2.jpeg";
+import project_img_3 from "./project_img_3.jpeg";
+import project_img_4 from "./project_img_4.jpeg";
+import project_img_5 from "./project_img_5.jpeg";
+import project_img_6 from "./project_img_6.jpeg";
+import project_img_7 from "./project_img_7.jpeg";
+import project_img_8 from "./project_img_8.jpeg";
+import project_img_9 from "./project_img_9.jpeg";
+import project_img_10 from "./project_img_10.jpeg";
+import project_img_11 from "./project_img_11.jpg";
+
 
 export const assets = {
-    filipap,
-    filipan,
-    logo,
-    header_img,
-    logo_dark,
-    cross_icon,
-    menu_icon,
-    star_icon,
-    brand_img,
-    project_img_1,
-    project_img_2,
-    project_img_3,
-    project_img_4,
-    left_arrow,
-    right_arrow,
-}
+  filipap,
+  filipan,
+  logo,
+  header_img,
+  logo_dark,
+  cross_icon,
+  menu_icon,
+  star_icon,
+  brand_img,
+  project_img_1,
+  project_img_2,
+  project_img_3,
+  project_img_4,
+  project_img_5,
+  project_img_6,
+  project_img_7,
+  project_img_8,
+  project_img_9,
+  project_img_10,
+  project_img_11,
+  left_arrow,
+  right_arrow,
+};
 
 export const projectsData = [
-    {
-      title: "Skyline Haven",
-      price: "$2,50,000",
-      location: "California",
-      image: project_img_1
-    },
-    {
-      title: "Vista Verde",
-      price: "$2,50,000",
-      location: "San Francisco",
-      image: project_img_2
-    },
-    {
-      title: "Serenity Suites",
-      price: "$2,50,000",
-      location: "Chicago",
-      image: project_img_3
-    },
-    {
-      title: "Central Square",
-      price: "$2,50,000",
-      location: "Los Angeles",
-      image: project_img_4
-    },
-    {
-      title: "Vista Verde",
-      price: "$2,50,000",
-      location: "San Francisco",
-      image: project_img_5
-    },
-    {
-      title: "Serenity Suites",
-      price: "$2,50,000",
-      location: "Chicago",
-      image: project_img_6
-    },
-    
-  ];
+  {
+    title: "Bar Casa Blanca",
 
-  export const testimonialsData = [
-    {
-        name: "Donald Jackman",
-        title: "Marketing Manager",
-        image: profile_img_1,
-        alt: "Portrait of Donald Jackman",
-        rating: 5,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
-    },
-    {
-        name: "Richard Nelson",
-        title: "UI/UX Designer",
-        image: profile_img_2,
-        alt: "Portrait of Richard Nelson",
-        rating: 4,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
-    },
-    {
-        name: "James Washington",
-        title: "Co-Founder",
-        image: profile_img_3,
-        alt: "Portrait of James Washington",
-        rating: 5,
-        text: "From the very first meeting, they understood my vision and helped me find the perfect property. Their attention to detail and commitment to client satisfaction is unmatched."
-    }
+    image: project_img_1,
+  },
+  {
+    title: "Quincho 'El Encuentro' ",
+
+    image: project_img_2,
+  },
+  {
+    title: "Vidriera Local de Niños",
+
+    image: project_img_3,
+  },
+  {
+    title: "Vidriera Local de Indumentaria",
+
+    image: project_img_4,
+  },
+  {
+    title: "La habitación de Bianca",
+
+    image: project_img_5,
+  },
+  {
+    title: "Salon de belleza y estética",
+
+    image: project_img_6,
+  },
+  {
+    title: "Retrato de Toti",
+
+    image: project_img_7,
+  },
+  {
+    title: "Salon de eventos",
+
+    image: project_img_8,
+  },
+  {
+    title: "Living familiar",
+
+    image: project_img_9,
+  },
+  {
+    title: "Bar",
+
+    image: project_img_10,
+  },
+  {
+    title: "El retrato de Tori",
+
+    image: project_img_11,
+  },
 ];
+
+
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,36 @@
-import { assets } from "../assets/assets";
+import { useState } from "react";
+import { assets, projectsData } from "../assets/assets";
+import { useEffect } from "react";
 
 const Projects = () => {
+
+const [currentIndex, setCurrentIndex] = useState(0);
+const [cardToShow, setCardsToShow] = useState(1);
+
+useEffect(()=>{
+  const updateCardsToShow = ()=> {
+    if(window.innerWidth >= 1024){
+      setCardsToShow(projectsData.length);
+    }else {
+      setCardsToShow(1)
+    }
+  };
+    updateCardsToShow();
+    window.addEventListener('resize', updateCardsToShow);
+    return ()=> window.removeEventListener('resize', updateCardsToShow);
+  
+},[])
+
+
+
+
+const nextProject = ()=>{
+  setCurrentIndex((prevIndex)=> (prevIndex +1) % projectsData.length)
+}
+const prevProject = ()=>{
+  setCurrentIndex((prevIndex) => prevIndex === 0 ? projectsData.length -1: prevIndex -1)
+}
+
   return (
     <div
       className="container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full
@@ -9,7 +39,7 @@ const Projects = () => {
     >
       <h1 className="text-2xl sm:text-4xl  mb-2 text-center catamaran text-yellow font-bold">
         Proyectos{" "}
-        <span className="underline underline-offset-4 decoration-1 under font-light font-semibold">
+        <span className="underline underline-offset-4 decoration-1 under  font-semibold">
           Realizados
         </span>
       </h1>
@@ -19,24 +49,46 @@ const Projects = () => {
       {/*slider buttons*/}
       <div className="flex justify-end items-center mb-8">
         <button
+        onClick={prevProject}
           className="p-3 bg-gray-200 rounded mr-2"
           aria-label="Previous Project"
         >
           <img src={assets.left_arrow} alt="Previous" />
         </button>
         <button
+        onClick={nextProject}
           className="p-3 bg-gray-200 rounded mr-2"
           aria-label="Previous Project"
         >
           <img src={assets.right_arrow} alt="Next" />
         </button>
       </div>
-      {/*slider container hora del video 1:20:00*/}
-      <div>
 
+      {/*slider container hora del video 1:40:00*/}
+      <div className="overflow-hidden">
+        <div className="flex gap-8 transition-transform duration-500 ease-in-out"
+        style={{transform:`translateX(-${(currentIndex *100)/ cardToShow}%)`}}
+        >
+          {projectsData.map((project, index)=>(
+            <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
+              <img src={project.image} alt={project.title} className="w-full h-auto mb-14"/>
+              <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                <div className="inline-block bg-white w-3/4 px-4 py-2 shadow-md">
+                <h2 className="catamaran text-md font-semibold text-gray-800">
+                  {project.title}
+
+                </h2>
+                </div>
+
+              </div>
+            </div>
+          ))}
+
+        </div>
       </div>
     </div>
   );
 };
 
 export default Projects;
+
