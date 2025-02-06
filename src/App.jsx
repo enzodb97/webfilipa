fa31c4eddf1809f3cbe1301f631ac1c5980710a8
Enzo Daniel Bertolusso <151404661+enzodb97@users.noreply.github.com>
2025-02-05 23:27:11 -0300

    add metadata

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
+import { Helmet } from "react-helmet-async";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Pricing from "./components/Pricing"; 
+import Pricing from "./components/Pricing";
 import Newsletter from "./components/Newsletter";
 import BlogSection from "./components/BlogSection";
-
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      {/* Metadatos dinámicos con Helmet */}
+      <Helmet>
+        <title>Mi Sitio con React y Vite</title>
+        <meta name="author" content="Scriptal - Enzo Bertolusso" />
+        <meta name="keywords" content="pintura, murales, a mano, filipa, retrato de mascotas, envíos a todo el país, Rosario Santa Fe" />
+        <meta name="description" content="Arte personalizado: murales, retratos de mascotas y más. Envíos a todo el país desde Rosario, Santa Fe." />
+        <link rel="canonical" href="filipa-tau.vercel.app" />
+
+      </Helmet>
+
+      {/* Componentes principales */}
       <Header />
       <About />
       <Projects />
-      <BlogSection/>
+      <BlogSection />
       <Pricing />
       <Contact />
-      <Newsletter/>
+      <Newsletter />
       <Footer />
     </div>
   );
